cdb1db9914e0d8dc805eedf85063b693af1925aa
TreasureSam <111035492+TreasureSam@users.noreply.github.com>
2024-02-28 17:24:52 +0100

    simple shell

--- execute_prompt.c
+++ execute_prompt.c
@@ -16,3 +16,38 @@ void execute_command(const char *command) {
     }
     return; // Added semicolon here
 }
+
+
+#include "shell.h"
+
+void execute_command(const char *command)
+{
+    pid_t child_pid = fork();
+
+    if (child_pid == -1)
+    {
+        sam_print("Error forking process.\n");
+        exit(EXIT_FAILURE);
+    } else if (child_pid == 0)
+    {
+       char *args[128];
+       int arg_count = 0;
+
+       cahr *token = strtok((char*)command, " ");
+       while (token !=NULL) {
+        args[arg_count++] = token;
+        token = strtok(NULL, " ");
+       }
+    args[arg_count] = NULL;
+
+    execvp(args[0], args);
+
+    sam_print("Error executing command.\n");
+    exit(EXIT_FAILURE);
+    }
+    else
+    {
+        wait(NULL);
+    }
+
+}
